e5aa89b5dad595a337240abbadc0680c5deacc52
Secară Lucas <lucassecara@yahoo.com>
2025-07-03 13:59:07 +0300

    Started working on refreshing a website

---
+++ pannonia.ro/pannonia/galerie.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Casa Pannonia - Photo Gallery</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<link rel="stylesheet" type="text/css" href="css/style.css"  />
+<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="css/jquery.fancybox-buttons.css" />	
+</head>
+<body>
+<div class="main">
+	<div class="header">
+		<div style="padding-top:15px; float:left;"><a href="http://pannonia.ro/"><img src="images/logotop.gif" style="width:250px; border:0;" alt="Casa Pannonia" /></a></div>	
+		<div class="menu">
+		  <ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="galerie.php" class="active">Gallery</a></li>
+			<li><a href="contactus.php">Contact Us</a></li>
+		  </ul>
+		</div>
+	</div>
+	<div class="clr"></div>
+	<div class="body">
+		<div class="main_body_all" style="min-height:810px; float:left;">
+		  <h2>Gallery</h2>		
+			<!-- <a class="fancybox-buttons group" data-fancybox-group="button" href="images/top_2_big.jpg" title=""><img src="images/top_2.jpg" style="width:550px; height:314px; border:none;"></a>
+			<div class="clr" style="padding-bottom:10px;"></div>-->
+			<div style="width:100%; text-align:center;"><iframe width="800" height="450" src="https://www.youtube.com/embed/QIwPspqq9QI?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></div>
+			<div class="clr" style="padding-bottom:10px;"></div>
+			<div style="padding-bottom:20px; clear:both;">
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_1.JPG" title=""><img src="images/pannonia_ap_s_1.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_2.JPG" title=""><img src="images/pannonia_ap_s_2.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_3.JPG" title=""><img src="images/pannonia_ap_s_3.jpg" style="width:265px; height:176px; border:none;" /></a>
+			</div>	
+			<div class="clr" style="padding-bottom:10px;"></div>
+			<div style="padding-bottom:20px; clear:both;">
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_4.JPG" title=""><img src="images/pannonia_ap_s_4.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_5.JPG" title=""><img src="images/pannonia_ap_s_5.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_6.JPG" title=""><img src="images/pannonia_ap_s_6.jpg" style="width:265px; height:176px; border:none;" /></a>				
+			</div>	
+			<div class="clr" style="padding-bottom:10px;"></div>
+			<div style="padding-bottom:20px; clear:both;">
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_7.JPG" title=""><img src="images/pannonia_ap_s_7.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_8.JPG" title=""><img src="images/pannonia_ap_s_8.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_9.JPG" title=""><img src="images/pannonia_ap_s_9.jpg" style="width:265px; height:176px; border:none;" /></a>
+			</div>	
+			<div class="clr" style="padding-bottom:10px;"></div>
+			<div style="padding-bottom:20px; clear:both;">
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_10.JPG" title=""><img src="images/pannonia_ap_s_10.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_11.JPG" title=""><img src="images/pannonia_ap_s_11.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_12.JPG" title=""><img src="images/pannonia_ap_s_12.jpg" style="width:265px; height:176px; border:none;" /></a>				
+			</div>	
+			<div class="clr" style="padding-bottom:10px;"></div>
+			<div style="padding-bottom:20px; clear:both;">
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_13.JPG" title=""><img src="images/pannonia_ap_s_13.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_14.JPG" title=""><img src="images/pannonia_ap_s_14.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_15.JPG" title=""><img src="images/pannonia_ap_s_15.jpg" style="width:265px; height:176px; border:none;" /></a>
+			</div>	
+			<div class="clr" style="padding-bottom:10px;"></div>
+			<div style="padding-bottom:20px; clear:both;">
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_16.JPG" title=""><img src="images/pannonia_ap_s_16.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_17.JPG" title=""><img src="images/pannonia_ap_s_17.jpg" style="width:265px; height:176px; padding-right:10px; border:none;" /></a>
+				<a class="fancybox-buttons group" data-fancybox-group="button" href="images/pannonia_ap_18.JPG" title=""><img src="images/pannonia_ap_s_18.jpg" style="width:265px; height:176px; border:none;" /></a>
+			</div>	
+			<div class="clr"></div>			
+		</div>
+		<div class="parteneri" style="float:right; min-height:810px;">
+			<div style="height:25px;text-align:center; background-color: #5F2D0B; color:#fff; margin-bottom:10px; padding-top:10px; "><strong>parteneri</strong></div>
+			<div class="clr"></div>
+			<div style="padding:10px 0 10px 0px; border-bottom:1px solid #CCC;">
+				<script type="text/javascript" src="http://www.infopensiuni.ro/cod/cod2.js"></script><a title="Cazare Romania" href="http://www.infopensiuni.ro"> Cazare Romania</a>
+				<div class="clr"></div>
+			</div>
+			<div class="clr" style="padding:10px 0;"></div>	
+			<div style="width:100px; text-align:center; display:block;padding:10px">
+				<!-- CentruTuristic.ro v.1 --> <div> <table CELLPADDIN=0 CELLSPACING=0 border=0 width=100px> <tr> <td align="center"> <a href="http://www.centruturistic.ro/" title="Pensiuni, hoteluri si vile Romania" target=blank><img src="https://scontent.fotp3-3.fna.fbcdn.net/v/t1.0-9/11953194_887251804689040_94173731343307073_n.png?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=9u6zSGCR2fgAX86bXOV&_nc_ht=scontent.fotp3-3.fna&oh=7d376b6ea869415ae51066fd811215d6&oe=5F05DBA8" style="width:100%; height:auto;"></a> </td> </tr> <tr> <td align=center> <a href="http://www.centruturistic.ro/satu_mare/cazare_satu_mare.html" target=blank title="Pensiuni si hoteluri Satu Mare"> <font color=#04398F size=2> Cazare Satu Mare </font> </a> </td> </tr> </table> </div><!-- CentruTuristic.ro v.1 -->
+				<div class="clr"></div>
+			</div>
+			<div class="clr"></div>
+		</div>			
+		<div class="clr"></div>	
+	</div>
+</div>
+<div class="clr"></div>
+<div class="footer_block">
+	<div class="footer"> © Copyright 2012. www.pannonia.ro. All Rights Reserved. Website made by balan ciprian<br />
+    <a href="index.php">Home</a> | <a href="galerie.php">Gallery</a> | <a href="contactus.php">Contact US</a></div>
+<!-- Quantcast Choice. Consent Manager Tag v2.0 (for TCF 2.0) -->
+<script type="text/javascript" async=true>
+(function() {
+  var host = window.location.hostname;
+  var element = document.createElement('script');
+  var firstScript = document.getElementsByTagName('script')[0];
+  var milliseconds = new Date().getTime();
+  var url = 'https://quantcast.mgr.consensu.org'
+    .concat('/choice/', '2z86hP6SXQLhv', '/', host, '/choice.js')
+    .concat('?timestamp=', milliseconds);
+  var uspTries = 0;
+  var uspTriesLimit = 3;
+  element.async = true;
+  element.type = 'text/javascript';
+  element.src = url;
+
+  firstScript.parentNode.insertBefore(element, firstScript);
+
+  function makeStub() {
+    var TCF_LOCATOR_NAME = '__tcfapiLocator';
+    var queue = [];
+    var win = window;
+    var cmpFrame;
+
+    function addFrame() {
+      var doc = win.document;
+      var otherCMP = !!(win.frames[TCF_LOCATOR_NAME]);
+
+      if (!otherCMP) {
+        if (doc.body) {
+          var iframe = doc.createElement('iframe');
+
+          iframe.style.cssText = 'display:none';
+          iframe.name = TCF_LOCATOR_NAME;
+          doc.body.appendChild(iframe);
+        } else {
+          setTimeout(addFrame, 5);
+        }
+      }
+      return !otherCMP;
+    }
+
+    function tcfAPIHandler() {
+      var gdprApplies;
+      var args = arguments;
+
+      if (!args.length) {
+        return queue;
+      } else if (args[0] === 'setGdprApplies') {
+        if (
+          args.length > 3 &&
+          args[2] === 2 &&
+          typeof args[3] === 'boolean'
+        ) {
+          gdprApplies = args[3];
+          if (typeof args[2] === 'function') {
+            args[2]('set', true);
+          }
+        }
+      } else if (args[0] === 'ping') {
+        var retr = {
+          gdprApplies: gdprApplies,
+          cmpLoaded: false,
+          cmpStatus: 'stub'
+        };
+
+        if (typeof args[2] === 'function') {
+          args[2](retr);
+        }
+      } else {
+        queue.push(args);
+      }
+    }
+
+    function postMessageEventHandler(event) {
+      var msgIsString = typeof event.data === 'string';
+      var json = {};
+
+      try {
+        if (msgIsString) {
+          json = JSON.parse(event.data);
+        } else {
+          json = event.data;
+        }
+      } catch (ignore) {}
+
+      var payload = json.__tcfapiCall;
+
+      if (payload) {
+        window.__tcfapi(
+          payload.command,
+          payload.version,
+          function(retValue, success) {
+            var returnMsg = {
+              __tcfapiReturn: {
+                returnValue: retValue,
+                success: success,
+                callId: payload.callId
+              }
+            };
+            if (msgIsString) {
+              returnMsg = JSON.stringify(returnMsg);
+            }
+            event.source.postMessage(returnMsg, '*');
+          },
+          payload.parameter
+        );
+      }
+    }
+
+    while (win) {
+      try {
+        if (win.frames[TCF_LOCATOR_NAME]) {
+          cmpFrame = win;
+          break;
+        }
+      } catch (ignore) {}
+
+      if (win === window.top) {
+        break;
+      }
+      win = win.parent;
+    }
+    if (!cmpFrame) {
+      addFrame();
+      win.__tcfapi = tcfAPIHandler;
+      win.addEventListener('message', postMessageEventHandler, false);
+    }
+  };
+
+  if (typeof module !== 'undefined') {
+    module.exports = makeStub;
+  } else {
+    makeStub();
+  }
+
+  var uspStubFunction = function() {
+    var arg = arguments;
+    if (typeof window.__uspapi !== uspStubFunction) {
+      setTimeout(function() {
+        if (typeof window.__uspapi !== 'undefined') {
+          window.__uspapi.apply(window.__uspapi, arg);
+        }
+      }, 500);
+    }
+  };
+
+  var checkIfUspIsReady = function() {
+    uspTries++;
+    if (window.__uspapi === uspStubFunction && uspTries < uspTriesLimit) {
+      console.warn('USP is not accessible');
+    } else {
+      clearInterval(uspInterval);
+    }
+  };
+
+  if (typeof window.__uspapi === 'undefined') {
+    window.__uspapi = uspStubFunction;
+    var uspInterval = setInterval(checkIfUspIsReady, 6000);
+  }
+})();
+</script>
+<!-- End Quantcast Choice. Consent Manager Tag v2.0 (for TCF 2.0) -->
+</div>
+<div class="clr"></div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" type="text/javascript"></script>	
+<script type="text/javascript" src="js/jquery.fancybox.js"></script>
+<script type="text/javascript" src="js/jquery.fancybox-buttons.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('.fancybox').fancybox();
+		$('.fancybox-buttons').fancybox({
+			openEffect  : 'none',
+			closeEffect : 'none',
+			prevEffect : 'none',
+			nextEffect : 'none',
+			closeBtn  : false,
+			helpers : {
+				title : {
+					type : 'inside'
+				},
+				buttons	: {}
+			},
+			afterLoad : function() {
+				this.title = 'Image ' + (this.index + 1) + ' of ' + this.group.length + (this.title ? ' - ' + this.title : '');
+			}
+		});
+	});
+</script>
+</body>
+</html>
---
+++ pannonia.ro/pannonia/index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Pannonia Apartments</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="main">
+	<div class="header">
+		<div style="padding-top:15px; float:left;"><a href="http://pannonia.ro/"><img src="images/logotop.gif" style="width:250px; border:0;" alt="Casa Pannonia" /></a></div>
+		<div class="menu">
+			<ul>
+				<li><a href="index.php" class="active">Home</a></li>
+				<li><a href="galerie.php">Gallery</a></li>
+				<li><a href="contactus.php">Contact US</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="clr"></div>
+	<div class="body">
+		<div style="width:250px; float:left; margin:0 10px;">
+			<div class="logo"><img src="images/logo.jpg" style="width:250px; height:250px;" alt="picture" /> </div>
+			<div class="side_body">
+				<div>
+					<div style="text-align:center;width:250px; padding-bottom:10px;"><strong>
+					Strada: Dsida Jeno, Nr.: 18<br />
+					440050 Satu Mare, Romania<br />
+					Phone: +40 725.99.82.78
+					</strong></div>
+					<div style="text-align:center;width:250px; "><a href="http://www.booking.com/hotel/ro/casa-pannonia.html?aid=330843;lang=ro;pb=1" target="booking"><img src="//aff.bstatic.com/images/affiliate/330843/booknow_ro.gif" alt="Rezervaţi acum" style="border: 0; padding-left:40px;" /></a>
+						<div style="clear:both;">Oferit de Booking.com</div>
+					</div>
+					<div style="clear:both; padding-top:10px;">
+						<div class="fb-like-box" data-href="https://www.facebook.com/CPannonia" data-width="250" data-height="400" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
+					</div>
+				</div>
+				<div class="clr"></div>
+			</div>
+			<div class="clr"></div>
+		</div>
+		<div class="main_body" style="min-height:810px; float:left;">
+			<img src="images/images1_index.jpg" style="width:550px; height:335px; padding-bottom:20px;">
+			<h2>Pannonia Apartments</h2>
+			<p>The Apartments are located at a 1km walk from the centre of Satu Mare in a quiet residential area. They are fully furnished to the highest standard. Each kitchen is fully equiped with fridge, freezer, microwave oven, gas hob and oven also having filter coffee making facility.</p>
+			<img src="images/top_1.jpg" style="width:550px; height:150px; padding-bottom:20px;">
+			<p>The apartments range between 60m^2 and 75m^2.<br/>
+				All with modern new furniture, Satalite TV, internet, LCD television, DVD, and central heating<br/>
+				The apartments are available to rent for periods from 1 night.<br/>
+				The apartments are fully serviced, with optional extras including catering and laundary services possible<br/>
+				Secure parking is available and included in the price,<br/>
+				Car hire can be arranged (not included in the price) </p>
+			<div class="clr"></div>
+		</div>
+		<div class="parteneri" style="float:right; min-height:810px; width:120px;">
+			<div style="height:25px;text-align:center; background-color: #5F2D0B; color:#fff; margin-bottom:10px; padding-top:10px; "><strong>parteneri</strong></div>
+			<div class="clr"></div>
+			<div style="padding:10px 0 10px 0px; border-bottom:1px solid #CCC;">
+				<script type="text/javascript" src="http://www.infopensiuni.ro/cod/cod2.js"></script><a title="Cazare Romania" href="http://www.infopensiuni.ro"> Cazare Romania</a>
+				<div class="clr"></div>
+			</div>
+			<div class="clr" style="padding:10px 0;"></div>	
+			<div style="width:100px; text-align:center; display:block;padding:10px">
+				<!-- CentruTuristic.ro v.1 --> <div> <table CELLPADDIN=0 CELLSPACING=0 border=0 width=100px> <tr> <td align="center"> <a href="http://www.centruturistic.ro/" title="Pensiuni, hoteluri si vile Romania" target=blank><img src="https://scontent.fotp3-3.fna.fbcdn.net/v/t1.0-9/11953194_887251804689040_94173731343307073_n.png?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=9u6zSGCR2fgAX86bXOV&_nc_ht=scontent.fotp3-3.fna&oh=7d376b6ea869415ae51066fd811215d6&oe=5F05DBA8" style="width:100%; height:auto;"></a> </td> </tr> <tr> <td align=center> <a href="http://www.centruturistic.ro/satu_mare/cazare_satu_mare.html" target=blank title="Pensiuni si hoteluri Satu Mare"> <font color=#04398F size=2> Cazare Satu Mare </font> </a> </td> </tr> </table> </div><!-- CentruTuristic.ro v.1 -->
+				<div class="clr"></div>
+			</div>
+			<div class="clr"></div>
+		</div>
+		<div class="clr"></div>
+	</div>
+</div>
+<div class="clr"></div>
+<div class="footer_block">
+	<div class="footer"> © Copyright 2012. www.pannonia.ro. All Rights Reserved. Website made by balan ciprian<br />
+    <a href="index.php">Home</a> | <a href="galerie.php">Gallery</a> | <a href="contactus.php">Contact US</a></div>
+<!-- Quantcast Choice. Consent Manager Tag v2.0 (for TCF 2.0) -->
+<script type="text/javascript" async=true>
+(function() {
+  var host = window.location.hostname;
+  var element = document.createElement('script');
+  var firstScript = document.getElementsByTagName('script')[0];
+  var milliseconds = new Date().getTime();
+  var url = 'https://quantcast.mgr.consensu.org'
+    .concat('/choice/', '2z86hP6SXQLhv', '/', host, '/choice.js')
+    .concat('?timestamp=', milliseconds);
+  var uspTries = 0;
+  var uspTriesLimit = 3;
+  element.async = true;
+  element.type = 'text/javascript';
+  element.src = url;
+
+  firstScript.parentNode.insertBefore(element, firstScript);
+
+  function makeStub() {
+    var TCF_LOCATOR_NAME = '__tcfapiLocator';
+    var queue = [];
+    var win = window;
+    var cmpFrame;
+
+    function addFrame() {
+      var doc = win.document;
+      var otherCMP = !!(win.frames[TCF_LOCATOR_NAME]);
+
+      if (!otherCMP) {
+        if (doc.body) {
+          var iframe = doc.createElement('iframe');
+
+          iframe.style.cssText = 'display:none';
+          iframe.name = TCF_LOCATOR_NAME;
+          doc.body.appendChild(iframe);
+        } else {
+          setTimeout(addFrame, 5);
+        }
+      }
+      return !otherCMP;
+    }
+
+    function tcfAPIHandler() {
+      var gdprApplies;
+      var args = arguments;
+
+      if (!args.length) {
+        return queue;
+      } else if (args[0] === 'setGdprApplies') {
+        if (
+          args.length > 3 &&
+          args[2] === 2 &&
+          typeof args[3] === 'boolean'
+        ) {
+          gdprApplies = args[3];
+          if (typeof args[2] === 'function') {
+            args[2]('set', true);
+          }
+        }
+      } else if (args[0] === 'ping') {
+        var retr = {
+          gdprApplies: gdprApplies,
+          cmpLoaded: false,
+          cmpStatus: 'stub'
+        };
+
+        if (typeof args[2] === 'function') {
+          args[2](retr);
+        }
+      } else {
+        queue.push(args);
+      }
+    }
+
+    function postMessageEventHandler(event) {
+      var msgIsString = typeof event.data === 'string';
+      var json = {};
+
+      try {
+        if (msgIsString) {
+          json = JSON.parse(event.data);
+        } else {
+          json = event.data;
+        }
+      } catch (ignore) {}
+
+      var payload = json.__tcfapiCall;
+
+      if (payload) {
+        window.__tcfapi(
+          payload.command,
+          payload.version,
+          function(retValue, success) {
+            var returnMsg = {
+              __tcfapiReturn: {
+                returnValue: retValue,
+                success: success,
+                callId: payload.callId
+              }
+            };
+            if (msgIsString) {
+              returnMsg = JSON.stringify(returnMsg);
+            }
+            event.source.postMessage(returnMsg, '*');
+          },
+          payload.parameter
+        );
+      }
+    }
+
+    while (win) {
+      try {
+        if (win.frames[TCF_LOCATOR_NAME]) {
+          cmpFrame = win;
+          break;
+        }
+      } catch (ignore) {}
+
+      if (win === window.top) {
+        break;
+      }
+      win = win.parent;
+    }
+    if (!cmpFrame) {
+      addFrame();
+      win.__tcfapi = tcfAPIHandler;
+      win.addEventListener('message', postMessageEventHandler, false);
+    }
+  };
+
+  if (typeof module !== 'undefined') {
+    module.exports = makeStub;
+  } else {
+    makeStub();
+  }
+
+  var uspStubFunction = function() {
+    var arg = arguments;
+    if (typeof window.__uspapi !== uspStubFunction) {
+      setTimeout(function() {
+        if (typeof window.__uspapi !== 'undefined') {
+          window.__uspapi.apply(window.__uspapi, arg);
+        }
+      }, 500);
+    }
+  };
+
+  var checkIfUspIsReady = function() {
+    uspTries++;
+    if (window.__uspapi === uspStubFunction && uspTries < uspTriesLimit) {
+      console.warn('USP is not accessible');
+    } else {
+      clearInterval(uspInterval);
+    }
+  };
+
+  if (typeof window.__uspapi === 'undefined') {
+    window.__uspapi = uspStubFunction;
+    var uspInterval = setInterval(checkIfUspIsReady, 6000);
+  }
+})();
+</script>
+<!-- End Quantcast Choice. Consent Manager Tag v2.0 (for TCF 2.0) -->
+</div>
+<div class="clr"></div>
+</body>
+</html>
